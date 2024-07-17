042d63b1e6d0341a27cf3ae165b7b2848d55e97b
LinoyGeva <linoy.geva@campus.technion.ac.il>
2024-07-17 20:45:20 +0300

    add iterator, fix small bug and making some test with int on remove,insert and iterator

--- SortedList.h
+++ SortedList.h
@@ -3,40 +3,40 @@
 #include <iostream>
 #include <stdexcept>
 
+
 template<typename T>
 class Node {
-private:
-    friend SortedList<T>;
-    friend SortedList<T>::ConstIterator;
+public:
     T value;
     Node *next;
     Node(const T &val);
     Node(const T &val, Node<T> *nextPtr);
     Node(const Node<T> &node);
     ~Node();
-    void insertNext(T &va);
-    void removeNext(Node<T> *node);
-};
 
+};
 
 namespace mtm {
 
     template<typename T>
     class SortedList {
-    public:
+    private:
         Node<T> *head;
         int size;
-        SortedList() = default;
-        SortedList(const SortedList<T> &list);
-        SortedList<T> &operator=(const SortedList<T> &list);
+    public:
+        SortedList();
+        SortedList(const SortedList &list);
+        SortedList<T> &operator=(const SortedList &list);
         ~SortedList();
 
         class ConstIterator;
 
-        void insert(T newValue);
-        void remove(ConstIterator iterator);
-        int length();
-
+        void insert(const T &newValue);
+        void remove(SortedList::ConstIterator iterator);
+        int length() const;
+        template<typename U>
+        friend std::ostream &
+        operator<<(std::ostream &output, const SortedList<U> &list);
 
         ConstIterator begin() const;
         ConstIterator end() const;
@@ -74,12 +74,13 @@ namespace mtm {
 
         ConstIterator(Node<T> *node) : ptr(node) {};
     public:
+        ConstIterator() = default;
+        ConstIterator(const ConstIterator &iterator) = default;
         ~ConstIterator() = default;
-        ConstIterator(ConstIterator &iterator) = default;
         ConstIterator &operator=(const ConstIterator &list) = default;
 
-        SortedList<T> *list;
-        friend SortedList<T>;
+        friend class SortedList<T>;
+
         ConstIterator &operator++();
         bool operator!=(const ConstIterator &other) const;
         const T &operator*();
@@ -104,5 +105,144 @@ namespace mtm {
 
 }
 
+
+template<typename T>
+Node<T>::Node(const T &val) : value(T(val)), next(nullptr) {
+}
+
+template<typename T>
+Node<T>::Node(const T &val, Node<T> *nextPtr) :value(T(val)),
+                                               next(nextPtr) {
+}
+
+template<typename T>
+Node<T>::Node(const Node<T> &node): value(node.value), next(nullptr) {
+    if (node.next != nullptr) {
+        next = new Node(*(node.next));
+    }
+}
+
+template<typename T>
+Node<T>::~Node() {
+    if (next != nullptr) {
+        delete next;
+    }
+}
+
+
+namespace mtm {
+    template<typename T>
+    SortedList<T>::SortedList():head(nullptr), size(0) {}
+
+    template<typename T>
+    SortedList<T>::SortedList(const SortedList<T> &list):
+            head(new Node<T>(list.head)), size(0) {}
+
+    template<typename T>
+    SortedList<T> &SortedList<T>::operator=(const SortedList<T> &list) {
+        if (this == &list) {
+            return *this;
+        }
+        delete this->head;
+        size = list.size;
+        head = new Node<T>(*list.head);
+        return *this;
+    }
+
+
+    template<typename T>
+    SortedList<T>::~SortedList() {
+        if (head != nullptr) {
+            delete head;
+        }
+    }
+
+
+    template<typename T>
+    void SortedList<T>::insert(const T &newValue) {
+        if (head == nullptr || newValue > this->head->value) {
+            head = new Node<T>(newValue, head);
+        } else {
+            Node<T> *ptr = this->head;
+            while (ptr->next != nullptr && ptr->next->value > newValue) {
+                ptr = ptr->next;
+            }
+            Node<T> *node = new Node<T>(newValue, ptr->next);
+            ptr->next = node;
+        }
+        size++;
+    }
+
+    template<typename T>
+    void SortedList<T>::remove(SortedList::ConstIterator iterator) {
+        ConstIterator currPtr = this->head;
+        if (currPtr != iterator) {
+            ++currPtr;
+            ConstIterator prevPtr = this->head;
+            while (currPtr != iterator) {
+                ++currPtr;
+                ++prevPtr;
+            }
+            prevPtr.ptr->next = currPtr.ptr->next;
+            currPtr.ptr->next = nullptr;
+            delete currPtr.ptr;
+        } else {
+            Node<T> *temp = head;
+            this->head = head->next;
+            delete temp;
+        }
+    }
+
+
+    template<typename T>
+    int SortedList<T>::length() const {
+        return this->size;
+    }
+
+    template<typename T>
+    std::ostream &operator<<(std::ostream &output, const SortedList<T> &list) {
+        output << "start" << std::endl;
+        for (T val: list) {
+            output << val << std::endl;
+        }
+        output << "end!" << std::endl;
+        return output;
+    }
+
+    template<typename T>
+    typename SortedList<T>::ConstIterator SortedList<T>::begin() const {
+        return SortedList<T>::ConstIterator(head);
+    }
+
+    template<typename T>
+    typename SortedList<T>::ConstIterator SortedList<T>::end() const {
+        return SortedList<T>::ConstIterator(nullptr);
+    }
+
+    template<class T>
+    typename SortedList<T>::ConstIterator &
+    SortedList<T>::ConstIterator::operator++() {
+        if (ptr != nullptr) {
+            ptr = ptr->next;
+        }
+        return *this;
+    }
+
+    template<typename T>
+    bool SortedList<T>::ConstIterator::operator!=(
+            const SortedList<T>::ConstIterator &other) const {
+        return ptr != other.ptr;
+    }
+
+
+    //k=chek throw
+    template<class T>
+    const T &SortedList<T>::ConstIterator::operator*() {
+        if (!ptr) {
+            throw std::out_of_range("Iterator out of range");
+        }
+        return this->ptr->value;
+    }
+}
 
 
--- ourTest.cpp
+++ ourTest.cpp
@@ -1,3 +1,28 @@
-//
-// Created by linoy on 16/07/2024.
-//
+
+#include <iostream>
+#include "SortedList.h"
+
+
+using std::cout;
+using std::endl;
+
+using namespace mtm;
+
+int main(void) {
+    SortedList<int> list;
+    cout << list.length() << endl;
+    int n = 3;
+    list.insert(n);
+    n = 7;
+    list.insert(n);
+    n = 5;
+    list.insert(n);
+    n = 1;
+    list.insert(n);
+    cout << list << endl;
+    SortedList<int>::ConstIterator it = list.begin();
+    ++it;
+    list.remove(it);
+    cout << list << endl;
+
+}
